76979b672919904fa8d9461c837457769dae34eb
anbreaker <antunez19@gmail.com>
2019-12-16 23:19:12 +0100

    init flask

---
+++ hello.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Decorador
+@app.route('/')
+def helloWorld():
+    return 'Hola, Mundo'
+
+
+@app.route('/otroRecurso')
+def otro():
+    return '''
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Mi pagina</title>
+        </head>
+        <body>
+            <h1>Mi saludo</h1>
+            <p>Hola, mundo</p>
+        </body>
+    </html>
+    '''

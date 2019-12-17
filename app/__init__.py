5b42244e85afefcf2d1d8afb7541dd458afce15b
anbreaker <antunez19@gmail.com>
2019-12-17 20:04:57 +0100

    Learning flask

--- app/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask	#1º
-app = Flask(__name__)  	#2º Este orden es esencial
-from app import routes	#3º
--- myApp/routes.py
+++ myApp/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from myApp import app
 from flask import render_template
 
 

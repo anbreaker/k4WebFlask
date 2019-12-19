9879a878d4a91ab6f9ddbcf78d1866f9f5252184
anbreaker <antunez19@gmail.com>
2019-12-19 23:32:26 +0100

    initiated flask

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,5 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from myapp import routes
+from flask import Flask     #1º
+app = Flask(__name__)  	    #2º Este orden es esencial
+from myapp import routes	#3º
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,33 +1,31 @@
 from myapp import app
-from flask import render_template
+from flask import render_template, request
 import csv
 
+'''
+leer el fichero sales10.csv y transformarlo en un diccionario
+'''
+
+fSales = open('./data/sales.csv', 'r')
+
+csvreader = csv.reader(fSales, delimiter=',')
+registros = []
+for linea in csvreader:
+    registros.append(linea)
+    print(linea)
+
+cabecera = registros[0]
+
+ventas = []
+for datos in registros[1:]:
+    d = {}
+    for ix, nombre_campo in enumerate(cabecera):
+        d[nombre_campo] = datos[ix]
+    ventas.append(d)
 
 
 @app.route("/")
 def index():
-    '''
-    leer el fichero sales10.csv y transformarlo en un diccionario
-    '''
-    
-    fSales = open('./data/sales10.csv','r')
-
-    csvreader = csv.reader(fSales, delimiter=',')
-    registros = []
-    for linea in csvreader:
-        registros.append(linea)
-        print(linea)
-
-    cabecera = registros[0]
-
-    ventas = []
-    for datos in registros[1:]:
-        d = {}
-        for ix, nombre_campo in enumerate(cabecera): 
-            d[nombre_campo] = datos[ix]
-        ventas.append(d)
-
-
     '''
     procesarlo para obtener los totales
     '''
@@ -38,7 +36,8 @@ def index():
             regAct['ingresos_totales'] += float(linea['ingresos_totales'])
             regAct['beneficios_totales'] += float(linea['beneficio'])
         else:
-            datos[linea['region']] = {'ingresos_totales': float(linea['ingresos_totales']), 'beneficios_totales': float(linea['beneficio'])}
+            datos[linea['region']] = {'ingresos_totales': float(
+                linea['ingresos_totales']), 'beneficios_totales': float(linea['beneficio'])}
     '''
     Finalmente devolvemos una lista de tuplas con la estructura
     [('Region', {'ingresos_totales': valor, 'beneficios_totales': valor}),...]
@@ -47,12 +46,25 @@ def index():
     for clave in datos:
         resultado.append((clave, datos[clave]))
 
-
     '''
     enviarlo a index.html
     '''
     return render_template('index.html', registros=resultado)
 
-@app.route("/detail")
+
+@app.route('/detail')
 def detail():
-    return render_template('detail.html')
+
+    datos = {}
+    region_name = request.values['region']
+    for linea in ventas:
+        if linea['region'] == region_name:
+            if linea['pais'] in datos:
+                regAct = datos[linea['pais']]
+                regAct['ingresos_totales'] += float(linea['ingresos_totales'])
+                regAct['beneficios_totales'] += float(linea['beneficio'])
+            else:
+                datos[linea['pais']] = {'ingresos_totales': float(
+                    linea['ingresos_totales']), 'beneficios_totales': float(linea['beneficio'])}
+
+    return render_template('detail.html', region=region_name, registros=datos)
